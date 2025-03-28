8e1f8fa9b6dfa03849486a9e9eed2953ffe8a625
tiago-bacelar <pereira.migueltiago@gmail.com>
2025-03-28 22:51:02 +0000

    add data models

---
+++ lib/bundle.ts
@@ -0,0 +1,41 @@
+class Bundle {
+  id: string;
+  //code: number;
+  description?: string;
+  instructions?: string;
+  items: BundleItem[];
+
+  constructor(id: string, description: string, instructions: string, items: BundleItem[]) {
+    this.id = id;
+    this.description = description;
+    this.instructions = instructions;
+    this.items = items;
+  }
+}
+
+
+class BundleItem {
+  quantity: number;
+  product: Product;
+
+  constructor(product: Product, quantity: number) {
+    this.product = product;
+    this.quantity = quantity;
+  }
+}
+
+
+//start/end date of discount
+class Product {
+  name: string;
+  basePrice: number;
+  price: number;
+  image?: string;
+
+  constructor(name: string, basePrice: number, price: number, image?: string) {
+    this.name = name;
+    this.price = price;
+    this.basePrice = basePrice;
+    this.image = image;
+  }
+}
