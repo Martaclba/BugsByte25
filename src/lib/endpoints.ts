d989eb6c90798ca9a7066ad06495a348a0e2ff30
Gerson Junior <ger_son_junior@live.com.pt>
2025-03-29 09:06:43 +0000

    home

--- src/lib/endpoints.ts
+++ src/lib/endpoints.ts
@@ -1,22 +1,32 @@
 import { Bundle, BundleItem, BundleOverview, Product } from "./bundle";
 import { User } from "./user";
 
-
-async function user(id: string): Promise<User> {
+export async function get_user(id: string): Promise<User> {
   return new User("Joaquim", "joaquim01");
 }
 
-async function bundles(user_id: string): Promise<BundleOverview[]> {
+export async function get_bundles(user_id: string): Promise<BundleOverview[]> {
   return [
-    new BundleOverview("12345", "Arroz de Tomate", "yummy", "uwu"),
+    new BundleOverview(
+      "12345",
+      "Arroz de Tomate",
+      "O arroz de tomate é um prato típico português, cremoso e aromático, feito com arroz cozido em molho de tomate temperado.",
+      "asd"
+    ),
   ];
 }
 
-async function bundle(user_id: string, bundle_id: string): Promise<Bundle> {
-  return new Bundle(new BundleOverview("12345", "Arroz de Tomate", "yummy", "uwu"), [
-    new BundleItem(new Product("tomate", 1.01, 1.01), 2),
-    new BundleItem(new Product("arroz", 2.01, 2.01)),
-    new BundleItem(new Product("cebola", 2.01, 1.99)),
-    new BundleItem(new Product("alho", 0.49, 0.49)),
-  ]);
+export async function get_bundle(
+  user_id: string,
+  bundle_id: string
+): Promise<Bundle> {
+  return new Bundle(
+    new BundleOverview("12345", "Arroz de Tomate", "yummy", "uwu"),
+    [
+      new BundleItem(new Product("tomate", 1.01, 1.01), 2),
+      new BundleItem(new Product("arroz", 2.01, 2.01)),
+      new BundleItem(new Product("cebola", 2.01, 1.99)),
+      new BundleItem(new Product("alho", 0.49, 0.49)),
+    ]
+  );
 }
