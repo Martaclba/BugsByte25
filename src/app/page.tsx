e8b0f3fac68760ac74562eba436759f000567dc8
Gerson Junior <ger_son_junior@live.com.pt>
2025-03-29 14:44:58 +0000

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Footer from "@/components/Footer";
 import { BundleOverview } from "@/lib/bundle";
 import { get_bundles } from "@/lib/endpoints";
 import Image from "next/image";
@@ -100,13 +101,7 @@ export default function Home() {
           </div>
         ))}
       </div>
-      <div className="w-full px-6 mt-10">
-          <div className="border-t border-black/20 py-2 flex flex-row justify-end items-center gap-2">
-            <p className="text-black text-xs">Made with love</p>
-            <p className="text-black text-xs">©</p>
-            <a href="https://www.youtube.com/watch?v=CXEIsUPpeI4&t=732s"><Image src="/wiwiwi.png" alt="Ver receita" width={32} height={32} className="cursor-pointer"/></a>
-          </div>
-      </div>
+      <Footer/>
     </div>
   );
 }
